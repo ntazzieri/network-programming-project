1c44126697c7393aea4b4c62e4f516e02d29e4d3
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2025-05-27 20:50:24 +0200

    refactor: update logging in HTTP server and modify navigation link in index.html

--- HTTPserver.py
+++ HTTPserver.py
@@ -9,24 +9,26 @@ srvSocket.bind(srvAddress)
 
 
 srvSocket.listen(1)
-print('The web server is ready to receive connections on port:', srvPort)
+print_and_log('=' * 50)
+print_and_log('Welcome')
+print_and_log('The web server is ready to receive connections on port:' + str(srvPort))
 
 notFoundPage = get_404_page()
 
 while True:
-    print('waiting for a connection...')
+    print_and_log('waiting for a connection...')
     connectionSocket, addr = srvSocket.accept()
-    print('Connection established with:', addr)
-    print('Connection socket:', connectionSocket)
+    print_and_log('Connection established with:'+ str(addr))
+    print_and_log('Connection socket:'+ str(connectionSocket))
     try:
         message = connectionSocket.recv(1024)
         if len(message.split()) > 0:
-            print(message, '::', message.split()[0], ':', message.split()[1])
+            print_and_log(str(message) + '::' + str(message.split()[0]) + ':' + str(message.split()[1]))
             filename = message.split()[1]
-            print(filename, '||', filename[1:])
+            print_and_log(str(filename) + '||' + str(filename[1:]))
             f = open(filename[1:], 'r')
             outputdata = f.read()
-            print(outputdata)
+            print_and_log(outputdata)
             f.close()
 
             # Send HTTP header with OK response
@@ -37,7 +39,7 @@ while True:
         # Send response message for file not found
         connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n", "UTF-8"))
         connectionSocket.send(bytes(notFoundPage, "UTF-8"))
-    print('Response sent successfully. Connection closed.')
+    print_and_log('Response sent successfully. Connection closed.')
     connectionSocket.close()
 
 
--- utilities.py
+++ utilities.py
@@ -1,6 +1,21 @@
 # Utility functions for the HTTP server
+
+import time
+
+'''
+It provides the 404 page from the file 'page404.html'.
+'''
 def get_404_page():
     f = open('page404.html', 'r')
     page = f.read()
     f.close()
     return page
+
+'''
+It prints the message on console and writes it into the file 'log/server.log'.
+'''
+def print_and_log(message):
+    print(message)
+    with open('log/server.log', 'a') as log:
+        log.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ': ')
+        log.write(message + '\r\n')
